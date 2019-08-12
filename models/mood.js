4868ecc0273cb283e9539cbfa1e8874c709d9678
asus <asus@asus.com>
2019-08-12 19:59:24 +0800

    update

--- models/mood.js
+++ models/mood.js
@@ -4,17 +4,16 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
-const daojuSchema = new Schema({
+const moodSchema = new Schema({
   createTime: String,
   des: String, // 心情文字
-  images: Array, // 图片
+  imageStrList: Array, // 图片
   videoPath: String, // 录音文件
-  type: Number, // 1心愿币修改卡0.5倍 1心愿币修改卡2倍 1心愿币修改卡3倍 4昵称修改卡 5 心愿换位卡
   id: Number,
   createdBy: String,
-  showOther: Boolean  
+  showOther: Boolean // 是否对TA可见
 })
 
-const Daoju = mongoose.model('Daoju', daojuSchema)
+const Mood = mongoose.model('Mood', moodSchema)
 
-export default Daoju
+export default Mood

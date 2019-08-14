01eaa7d9823db2741440752ddc450040735f7cea
asus <asus@asus.com>
2019-08-14 18:00:08 +0800

    update

--- routes/unAuth.js
+++ routes/unAuth.js
@@ -4,5 +4,5 @@ import User from '../controller/user'
 
 router.post('/v1/userLogin', User.login)
 router.post('/v1/rootLogin', User.rootLogin)
-router.post('/wechatLogin', User.wechatLogin)
+router.get('/wechatLogin', User.wechatLogin)
 module.exports = router

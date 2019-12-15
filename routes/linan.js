38c4018686fb34362ce0c97b4980a47389a3810a
asus <asus@asus.com>
2019-12-15 15:58:16 +0800

    update

--- routes/linan.js
+++ routes/linan.js
@@ -1,9 +1,10 @@
 var express = require('express')
 var router = express.Router()
 import Admin from '../controller/admin'
+import Linan from '../controller/linan'
 
-router.post('/v1/startSchedule', Admin.startSchedule)
-router.post('/v1/addYuanMoney', Admin.addYuanMoney)
-router.post('/v1/updateYuan', Admin.updateYuan)
 router.get('/v1/getYuan', Admin.getYuan)
+router.post('/v1/startSchedule', Admin.startSchedule)
+router.post('/v2/addJob', Linan.addJob)
+router.post('/v2/updateJob', Linan.updateJob)
 module.exports = router
--- routes/unAuth.js
+++ routes/unAuth.js
@@ -7,7 +7,7 @@ import Linan from '../controller/linan'
 router.post('/v1/adminLogin', Admin.adminLogin)
 router.post('/v1/wechatLogin', User.wechatLogin)
 router.post('/v2/login', Linan.login)
-router.post('/v2/getJob', Linan.getJob)
+router.get('/v2/getJob', Linan.getJob)
 router.post('/v1/wechatRegister', User.wechatRegister)
 router.post('/v1/wechatRegisterName', User.wechatRegisterName)
 
